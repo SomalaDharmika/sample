60a9a92c753b5bfd6ee44eb136712f39fde4714f
SomalaDharmika <somaladharmika@gmail.com>
2024-09-16 16:14:02 +0530

    components and functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/*this is class component*/
+/*import React, { Component } from "react";
 
-function App() {
+export default class App extends Component {
+  state = {
+    name: "sona",
+  };
+  render() {
+    return (
+      <div>
+        <h2>hi welcome {this.state.name}</h2>
+      </div>
+    );
+  }
+}*/
+
+/* functional component*/
+import React, { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("functioncompo");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>hi welcome {name}</h2>
     </div>
   );
-}
+};
 
 export default App;
